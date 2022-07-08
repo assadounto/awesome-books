88006ffb03ff69aa17c92ef752d9e8dff47a877e
rich <adukyerer@gmail.com>
2022-07-08 19:49:55 +0000

    add book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,22 @@
-import Addbook from './Addbook';
+import PropTypes from 'prop-types';
 
-const Books = () => (
-  <Addbook />
-);
+function Book(props) {
+  Book.propTypes = {
+    title: PropTypes.string,
+    author: PropTypes.string,
+  };
+  Book.defaultProps = {
+    title: 'My book',
+    author: 'Author Name',
+  };
+  const { title, author } = props;
+  return (
+    <div className="t">
+      {title}
+      {author}
+      <button type="submit">remove</button>
+    </div>
+  );
+}
 
-export default Books;
+export default Book;
